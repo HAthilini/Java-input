b87662d0a30f5c8af7837e096f9b427ae1aa3959
HAthilini <thiliniuthpala207@gmail.com>
2017-08-12 23:36:15 +0530

    first commit

---
+++ Example1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Example1{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		byte b=input.nextByte();
+		short s=input.nextShort();
+		int x=input.nextInt();
+		long y=input.nextLong();
+		
+		
+		float f;
+		f=input.nextFloat();
+		
+		
+		double d=input.nextDouble();
+		
+	
+		
+		boolean isPass;
+		isPass=input.nextBoolean();
+	}
+}
+
